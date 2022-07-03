f4950f5654c249db19f24bbabdf3f04f00f3e8a7
Yoga Pranata <ahmadzaylani001@gmail.com>
2022-07-03 19:51:28 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+
+with open("README.md", "r") as txt:
+    long_description = txt.read()
+
+setuptools.setup(
+    name='wibuapi',
+    version='0.0.1',
+    description='Wibu API Python Wrapper',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+    author='Yoga Pranata',
+    author_email='yoga@zyxdevs.eu.org',
+    url='https://github.com/zYxDevs/wibu-api-py.git',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent",
+    ],
+    install_requires= ['requests'],
+    python_requires='>=3.6'
+)
