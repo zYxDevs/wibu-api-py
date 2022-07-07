5977d33a98fe755f98c93b057ec365d2143cb4c4
Yoga Pranata <yoga@zyxdevs.eu.org>
2022-07-08 01:19:22 +0700

    v0.0.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as txt:
 
 setuptools.setup(
     name="wibuapi",
-    version="0.0.2",
+    version="0.0.3",
     description="Official Python Wrapper for Wibu API.",
     long_description=long_description,
     long_description_content_type="text/markdown",
