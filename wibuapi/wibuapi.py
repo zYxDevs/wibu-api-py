2866bb34d98906a99bc83af51b9dc04ae73d34e0
Yoga Pranata <yoga@zyxdevs.eu.org>
2022-07-08 18:08:43 +0700

    Update wibuapi.py

--- wibuapi/wibuapi.py
+++ wibuapi/wibuapi.py
@@ -79,59 +79,95 @@ class WibuAPI:
         Available tags: ahegao, waifu, neko, trap, bj
         """
         try:
-            if str(tags) not in ["ahegao", "waifu", "neko", "trap", "bj"]:
-                return (
-                    "Not a valid tags.\nAvailable tags: ahegao, waifu, neko, trap, bj"
-                )
-            elif str(category) != "nsfw":
-                return "Not a valid category.\nAvailable category: nsfw"
             url = f"{self.base_url}/anime/{category}/{tags}"
             response = get(url, timeout=5).json()
             return dumps(response, indent=2)
         except Exception as e:
             return f"An error occured report on @YBotsSupport\n\n{e}"
 
-    def genshin(self, path: str, query):
-        """
-        Available path: user, character, enemy, artifact, element, weapon
-        """
+    def giUser(self, uid: int):
         try:
-            if str(path) == "artifact":
-                url = f"{self.base_url}/game/gi/artifact?name={query}"
-            elif str(path) == "character":
-                url = f"{self.base_url}/game/gi/character?name={query}"
-            elif str(path) == "element":
-                url = f"{self.base_url}/game/gi/element?name={query}"
-            elif str(path) == "enemy":
-                url = f"{self.base_url}/game/gi/enemy?name={query}"
-            elif str(path) == "user":
-                url = f"{self.base_url}/game/gi/user?uid={query}"
-            elif str(path) == "weapon":
-                url = f"{self.base_url}/game/gi/weapon?name={query}"
-            else:
-                return "Not a valid path.\nAvailable path: user, character, enemy, artifact, element, weapon"
+            url = f"{self.base_url}/game/gi/user?uid={uid}"
+            response = get(url, timeout=5).json()
+            return dumps(response, indent=2)
+        except Exception as e:
+            return f"An error occured report on @YBotsSupport\n\n{e}"
+
+    def giWeapon(self, query: str):
+        try:
+            url = f"{self.base_url}/game/gi/weapon?name={query}"
+            response = get(url, timeout=5).json()
+            return dumps(response, indent=2)
+        except Exception as e:
+            return f"An error occured report on @YBotsSupport\n\n{e}"
+
+    def giEnemy(self, query: str):
+        try:
+            url = f"{self.base_url}/game/gi/enemy?name={query}"
+            response = get(url, timeout=5).json()
+            return dumps(response, indent=2)
+        except Exception as e:
+            return f"An error occured report on @YBotsSupport\n\n{e}"
+
+    def giElement(self, query: str):
+        try:
+            url = f"{self.base_url}/game/gi/element?name={query}"
+            response = get(url, timeout=5).json()
+            return dumps(response, indent=2)
+        except Exception as e:
+            return f"An error occured report on @YBotsSupport\n\n{e}"
+
+    def giCharacter(self, query: str):
+        try:
+            url = f"{self.base_url}/game/gi/character?name={query}"
+            response = get(url, timeout=5).json()
+            return dumps(response, indent=2)
+        except Exception as e:
+            return f"An error occured report on @YBotsSupport\n\n{e}"
+
+    def giArtifact(self, query: str):
+        try:
+            url = f"{self.base_url}/game/gi/artifact?name={query}"
             response = get(url, timeout=15).json()
             return dumps(response, indent=2)
         except Exception as e:
             return f"An error occured report on @YBotsSupport\n\n{e}"
 
-    def brawl(self, path: str, query: str):
-        """
-        Available path: player, playerlog, club, clubmember, event
-        """
+    def brawlClub(self, tag: str):
         try:
-            if str(path) == "club":
-                url = f"{self.deprecated_url}/game/brawl/club?clubTag={query}"
-            elif str(path) == "clubmember":
-                url = f"{self.deprecated_url}/game/brawl/club/member?clubTag={query}"
-            elif str(path) == "event":
-                url = f"{self.deprecated_url}/game/brawl/event"
-            elif str(path) == "player":
-                url = f"{self.deprecated_url}/game/brawl/player?playerTag={query}"
-            elif str(path) == "playerlog":
-                url = f"{self.deprecated_url}/game/brawl/player/log?playerTag={query}"
-            else:
-                return "Not a valid path.\nAvailable path: player, playerlog, club, clubmember, event"
+            url = f"{self.deprecated_url}/game/brawl/club?clubTag={query}"
+            response = get(url, timeout=15).json()
+            return dumps(response, indent=2)
+        except Exception as e:
+            return f"An error occured report on @YBotsSupport\n\n{e}"
+
+    def brawlClubMember(self, tag: str):
+        try:
+            url = f"{self.deprecated_url}/game/brawl/club/member?clubTag={tag}"
+            response = get(url, timeout=15).json()
+            return dumps(response, indent=2)
+        except Exception as e:
+            return f"An error occured report on @YBotsSupport\n\n{e}"
+
+    def brawlEvent(self):
+        try:
+            url = f"{self.deprecated_url}/game/brawl/event"
+            response = get(url, timeout=15).json()
+            return dumps(response, indent=2)
+        except Exception as e:
+            return f"An error occured report on @YBotsSupport\n\n{e}"
+
+    def brawlPlayer(self, tag: str):
+        try:
+            url = f"{self.deprecated_url}/game/brawl/player?playerTag={tag}"
+            response = get(url, timeout=15).json()
+            return dumps(response, indent=2)
+        except Exception as e:
+            return f"An error occured report on @YBotsSupport\n\n{e}"
+
+    def brawlPlayerLog(self, tag: str):
+        try:
+            url = f"{self.deprecated_url}/game/brawl/player/log?playerTag={query}"
             response = get(url, timeout=15).json()
             return dumps(response, indent=2)
         except Exception as e:
