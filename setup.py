a1156ba6da379e8f5daf7408b0437ab2c95f7dc7
Yoga Pranata <yoga@zyxdevs.eu.org>
2022-07-23 16:12:29 +0700

    v0.0.6
    - add urban dictionary
    - add youtube scraper
    - add wallpaper (alphacoders)

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as txt:
 
 setuptools.setup(
     name="wibuapi",
-    version="0.0.5",
+    version="0.0.6",
     description="Official Python Wrapper for Wibu API.",
     long_description=long_description,
     long_description_content_type="text/markdown",
