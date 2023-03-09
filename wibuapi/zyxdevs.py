752ecb94a5b511dfde485a549e7f598cca3c06ce
Yoga Pranata <yoga@zyxdevs.eu.org>
2023-03-09 08:36:11 +0700

    try

--- wibuapi/zyxdevs.py
+++ wibuapi/zyxdevs.py
@@ -7,17 +7,21 @@ from requests import get
 
 
 class Etc:
-    def stats(site: str):
+    def __init__(self):
+        self.base_url = "https://wibu-api.eu.org/api"
+        self.deprecated_url = "https://api.wibu-api.eu.org/api"
+
+    def stats(self, site: str):
         try:
-            url = "https://wibu-api.eu.org/api"
-            if site == "deprecated":
-                url = "https://api.wibu-api.eu.org/api"
+            url = self.base_url
+            if site == ["deprecated", "d"]:
+                url = self.deprecated_url
             response = get(url, timeout=15).json()
             return dumps(response, indent=2)
         except Exception as e:
             return f"An error occured report on @YBotsSupport\n\n{e}"
 
-    def xCode(url: str):
+    def xCode(self, url: str):
         """
         Personal site parser(lazy to make new repo) so i add this to here.
         https://xcodebasisdrive.blogspot.com
