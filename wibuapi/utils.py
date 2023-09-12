486988441a3bb548548b33a91d66182a441847cc
Yoga Pranata <yoga@zyxdevs.eu.org>
2023-09-12 23:38:52 +0700

    utilitiies file

---
+++ wibuapi/utils.py
@@ -0,0 +1,7 @@
+import aiohttp
+
+async def getwibu(url: str, timeout: int):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, timeout=timeout) as response:
+            response.raise_for_status()
+            return await response.json()
