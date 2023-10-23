1052625e0ac63c4c3c6206c34f58b62a52e15c88
Yoga Pranata <yoga@zyxdevs.eu.org>
2023-10-23 10:19:21 +0700

    Update wibuapi.py

--- wibuapi/wibuapi.py
+++ wibuapi/wibuapi.py
@@ -5,7 +5,7 @@ from requests import get
 
 
 class WibuAPI:
-    def __init__(self, apikey=None):
+    def __init__(self, apikey: str = None):
         self.base_url = "https://wibu-api.eu.org"
         self.deprecated_url = "https://api.wibu-api.eu.org"
         self.headers = {"x-wibu-key": apikey}
